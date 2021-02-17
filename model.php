185491896babc5ed53d072f739e435fe36fcacd1
Vadim Ponomarev <longinusjanitus@outlook.com>
2021-02-17 13:42:50 +1000

    Latest update

--- model.php
+++ model.php
@@ -37,16 +37,16 @@ $list = str_replace($dir.'/','',(glob($dir.'/*', GLOB_ONLYDIR)));
 ?>
 <html>
 <head>
-<link rel="shortcut icon" href="model.png" type="image/x-icon">
+<link rel="shortcut icon" href="model.png?rev=<?=time();?>" type="image/x-icon">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 <title>Model Portfolio</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style.css?rev=<?=time();?>">
 </head>
 <body>
 <div class='window'>
-<h1><a href=<?php echo 'favicon.png'; ?>>
-<img class='hover' style="width:92px;height:92px;" src=<?php echo 'favicon.png'; ?>></a><?php echo ' '.$name; ?></h1>
+<h1><a href="favicon.png?rev=<?=time();?>">
+<img class='hover' style="width:92px;height:92px;" src="<?php echo 'favicon.png'; ?>?rev=<?=time();?>"></a><?php echo ' '.$name; ?></h1>
 <?php echo 'Height: '.round($height, 2).' '.$unitsize; ?><br>
 <?php echo 'Weight: '.round($weight, 0).' '.$unitmass; ?><br>
 <?php echo 'Body sizes: '.$bodysizes; ?><br>
